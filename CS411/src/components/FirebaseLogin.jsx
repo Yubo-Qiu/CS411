6d8643f40a5478f4a6f42fe4f00ac9b757051033
Yubo-Qiu <yubo@bu.edu>
2023-10-21 00:36:56 -0400

    front end almost done

---
+++ CS411/src/components/FirebaseLogin.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef } from 'react';
+import { ui, uiConfig } from '../firebaseSetup';  // Adjust the path as needed
+
+function FirebaseLogin() {
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        if (containerRef.current) {
+            ui.start(containerRef.current, uiConfig);
+        }
+        return () => {
+            ui.reset();
+        };
+    }, []);
+
+    return <div ref={containerRef}></div>;
+}
+
+export default FirebaseLogin;
